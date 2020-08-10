021f3387020d6962f5199e1be83f5ed281f8556a
Conrado Cañas <conradocanas@gmail.com>
2020-08-10 12:14:59 -0300

    Arreglado como se muestra el JSON en front

--- vue.js
+++ vue.js
@@ -10,12 +10,12 @@ const app = new Vue({
         drawer: null,
         titulo: 'App - Que tomo?',
         goDark: true,
-        itemsPerPageArray: [4, 8, 12],
+        itemsPerPageArray: [6, 12, 18],
         search: '',
         filter: {},
         sortDesc: false,
         page: 1,
-        itemsPerPage: 4,
+        itemsPerPage: 6,
         sortBy: 'name',
         items: [],
         keys: []
@@ -44,32 +44,9 @@ const app = new Vue({
       },
       filteredKeys () {
         unusedKeys = ["idDrink", "strDrink", "strDrinkAlternate", "strDrinkES", "strDrinkDE", "strDrinkFR", "strDrinkZH-HANS", "strDrinkZH-HANT", "strTags", "strVideo", "strIBA",
-         "strInstructionsES", "strInstructionsDE", "strInstructionsFR", "strInstructionsZH-HANS", "strInstructionsZH-HANT", "strDrink", "strDrink", "strDrink"]
+         "strInstructionsES", "strInstructionsDE", "strInstructionsFR", "strInstructionsZH-HANS", "strInstructionsZH-HANT", "strCreativeCommonsConfirmed", "dateModified", "strDrinkThumb"]
         readyKeys = this.keys.filter(item => !unusedKeys.includes(item))
-      //   readyKeys = this.keys.filter(key => key !== "strDrink")
-      //   readyKeys = this.keys
-      //   for( i = 0; i < readyKeys.length; i++){
-      //     if(readyKeys[i] == "idDrink"
-      //     || readyKeys[i] == "strDrink"
-      //     || readyKeys[i] == "strDrinkAlternate"
-      //     || readyKeys[i] == "strDrinkDE"
-      //     || readyKeys[i] == "strDrinkES"
-      //     || readyKeys[i] == "strDrinkFR"
-      //     || readyKeys[i] == "strDrinkZH-HANS"
-      //     || readyKeys[i] == "strDrinkZH-HANT"
-      //     || readyKeys[i] == "strTags"
-      //     || readyKeys[i] == "strVideo"
-      //     || readyKeys[i] == "strIBA"
-      //     || readyKeys[i] == "strInstructionsES"
-      //     || readyKeys[i] == "strInstructionsDE"
-      //     || readyKeys[i] == "strInstructionsFR"
-      //     || readyKeys[i] == "strInstructionsZH-HANS"
-      //     || readyKeys[i] == "strInstructionsZH-HANT"
-      //     ){
-      //       delete readyKeys[i]
-      //     }
-      //   }
-      return readyKeys
+        return readyKeys
       }
     },
     mounted () {
